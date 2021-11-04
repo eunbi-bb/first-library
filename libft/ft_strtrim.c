c53412b2b43087803413ffcae556986a23346dde
eunbi-bb <80834766+eunbi-bb@users.noreply.github.com>
2021-11-04 19:57:17 +0100

    strtrim editted
    2 options

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -30,3 +30,34 @@ char	*ft_strtrim(char const *s1, char const *set)
 	str = ft_strdup(s1);
 	return (str);
 }
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	front;
+	size_t	back;
+	char	*str;
+
+	str = 0;
+	if (s1 && set)
+	{
+		front = 0;
+		back = ft_strlen(s1);
+		while (s1[front] && ft_strchr(set, s1[front]))
+			front++;
+		while (s1[back - 1] && ft_strchr(set, s1[back - 1]) && back > front)
+			back--;
+		str = (char *)malloc(sizeof(char) * (back - front + 1));
+		if (!str)
+			return (NULL);
+		if (str)
+			ft_strlcpy(str, &s1[front], back - front + 1);
+	}
+	return (str);
+}
+
+int main()
+{
+	printf("%s\n", ft_strtrim("abqbc", "abc"));
+	printf("%s\n", ft_strtrim("xavocadoyz", "xyz"));
+	return 0;
+}
