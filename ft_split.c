51809f1be65f3465d7d8b369469b08688c88f766
eunbi-bb <80834766+eunbi-bb@users.noreply.github.com>
2021-11-10 18:26:27 +0100

    split is added
    need to reduce lines

---
+++ ft_split.c
@@ -0,0 +1,61 @@
+#include "libft.h"
+#include<stdio.h>
+
+size_t	word_counter(char const *s, char c)
+{
+	size_t	count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			count++;
+			while (*s != '\0' && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	return (count);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t		i;
+	size_t		len;
+	const char	*start;
+	char		**str;
+
+	if (!s)
+		return (NULL);
+	str = (char **)malloc(sizeof(*str) * word_counter(s, c) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		start = s;
+		len = 0;
+		while (*s && *s != c)
+		{
+			s++;
+			len++;
+		}
+		if (*(s - 1) != c)
+			str[i++] = ft_substr(start, 0, len);
+	}
+	str[i] = 0;
+	return (str);
+}
+
+int	main(void)
+{
+	char	*splstr = "aim my mammoth at me";
+	printf("ft_split(\"%s\", 'm'):\n", splstr);
+	char	**split = ft_split(splstr, 'm');
+	while (*split)
+		printf("\"%s\"\n", *split++);
+}
